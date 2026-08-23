abec4f5814c4353bbe96f37e7abd431004e74e70
caio <caiovitorgg@hotmail.com>
2026-08-23 13:05:03 -0300

    Alterações na questao 3

--- tps/TP-1/QUESTAO_3/questao3.java
+++ tps/TP-1/QUESTAO_3/questao3.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class questao3 {
 
     public static boolean vogal(String frase){
@@ -44,33 +43,31 @@ public class questao3 {
         return true;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String frase;
-        do{
-            frase = sc.nextLine();
-            if(!frase.equals("FIM")){
-                if(vogal(frase)){
-                    System.out.print("SIM ");
-                } else {
-                    System.out.print("NAO ");
-                }
-                if(consoante(frase)){
-                    System.out.print("SIM ");
-                } else {
-                    System.out.print("NAO ");
-                }
-                if(inteiro(frase)){
-                    System.out.print("SIM ");
-                } else {
-                    System.out.print("NAO ");
-                }
-                if(real(frase)){
-                    System.out.println("SIM");
-                } else {
-                    System.out.println("NAO");
-                }
-            }
-        } while(!frase.equals("FIM"));
-        sc.close();
+    Scanner sc = new Scanner(System.in);
+    String frase = sc.nextLine(); // leitura escorva antes do while
+    while(!(frase.length()==3 && frase.charAt(0)=='F' && frase.charAt(1)=='I' && frase.charAt(2)=='M')){ // enquanto a frase nao for fim continua
+        if(vogal(frase)){
+            System.out.print("SIM ");
+        } else {
+            System.out.print("NAO ");
+        }
+        if(consoante(frase)){
+            System.out.print("SIM ");
+        } else {
+            System.out.print("NAO ");
+        }
+        if(inteiro(frase)){
+            System.out.print("SIM ");
+        } else {
+            System.out.print("NAO ");
+        }
+        if(real(frase)){
+            System.out.println("SIM");
+        } else {
+            System.out.println("NAO");
+        }
+        frase = sc.nextLine(); // le a proxima frase ate achar o fim
+    }
+    sc.close();
     }
 }
